c7e75a6c774c60aaa0f451e1850c17bc45a83521
Alexander Kozlov <asmailiks@gmail.com>
2016-06-09 10:56:01 +0300

    Change project version to 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject my-webapp "0.1.1-SNAPSHOT"
+(defproject my-webapp "0.1.2-SNAPSHOT"
   :description "Поиск по базе данных ОГК"
   :url "http://192.168.0.132/"
   :min-lein-version "2.0.0"
