0d865d400b459e2e37f41c9a6087d61c2dd7b6f6
Alexander Kozlov <asmailiks@gmail.com>
2016-09-29 11:04:51 +0300

    Project setup
    Add dependencies for repl oriented developement.

--- project.clj
+++ project.clj
@@ -2,20 +2,40 @@
   :description "Поиск по базе данных ОГК"
   :url "http://192.168.0.132/"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [
+                 [org.clojure/clojure "1.8.0"]
                  [selmer "1.0.4"]
                  [hiccup "1.0.5"]
                  [metosin/ring-http-response "0.6.5"]
                  [org.clojure/java.jdbc "0.6.1"]
+                 [org.clojure/tools.loggin "0.3.1"]
+                 [org.clojure/tools.cli "0.3.5"]
                  [com.h2database/h2 "1.4.189"]
-                 [compojure "1.5.1"]
+                 [compojure "1.5.0"]
+                 [ring/ring-servlet "1.4.0"]
                  [ring/ring-defaults "0.2.1"]
-                 [ring/ring-jetty-adapter "1.5.0"] 
+                 [luminus-nrepl "0.1.4"]
+                 [mount "0.1.10"]
+                 [cprop "0.1.8"]
                  ]
-  :main my-webapp.handler
-  :aot :all
-  :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler my-webapp.handler/app}
+  
+  :jvm-opts ["-server" "-Dconf=.lein-env"]
+  :main my-webapp.core
+  :plugins [[lein-uberwar "0.2.0"]
+            [lein-cprop "1.0.1"]]
+  
+  :uberwar
+  {:handler my-webapp.handler/app}
+  
   :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+  {:uberjar {:omit-source true
+             :aot :all
+             :source-paths ["env/prod/clj"]
+             :resource-paths ["env/prod/rosources"]}
+   
+   :dev {:dependencies [[ring/ring-mock "0.3.0"]
+                        [luminus-jetty "0.1.4"]
+                        [directory-naming/naming-java "0.8"]]
+         :repl-options {:init-ns user}
+         :source-paths ["env/dev/clj"]
+         :resource-paths ["env/dev/resources"]}})
