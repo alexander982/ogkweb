d1ef08195bd39acd8354c1ddba9025e570c1e9ec
Alexander Kozlov <asmailiks@gmail.com>
2016-10-17 11:29:58 +0300

    Fix dependencies

--- project.clj
+++ project.clj
@@ -8,18 +8,21 @@
                  [hiccup "1.0.5"]
                  [metosin/ring-http-response "0.6.5"]
                  [org.clojure/java.jdbc "0.6.1"]
-                 [org.clojure/tools.loggin "0.3.1"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/tools.cli "0.3.5"]
                  [com.h2database/h2 "1.4.189"]
-                 [compojure "1.5.0"]
+                 [compojure "1.5.1"]
                  [ring/ring-servlet "1.4.0"]
                  [ring/ring-defaults "0.2.1"]
                  [luminus-nrepl "0.1.4"]
+                 [luminus-log4j "0.1.3"]
                  [mount "0.1.10"]
                  [cprop "0.1.8"]
                  ]
   
   :jvm-opts ["-server" "-Dconf=.lein-env"]
+  :source-paths ["src"]
+  :resource-paths ["resources"]
   :main my-webapp.core
   :plugins [[lein-uberwar "0.2.0"]
             [lein-cprop "1.0.1"]]
@@ -32,10 +35,15 @@
              :aot :all
              :source-paths ["env/prod/clj"]
              :resource-paths ["env/prod/rosources"]}
+
+   :dev [:project/dev :profiles/dev]
    
-   :dev {:dependencies [[ring/ring-mock "0.3.0"]
-                        [luminus-jetty "0.1.4"]
-                        [directory-naming/naming-java "0.8"]]
-         :repl-options {:init-ns user}
-         :source-paths ["env/dev/clj"]
-         :resource-paths ["env/dev/resources"]}})
+   :project/dev {:dependencies [[ring/ring-mock "0.3.0"]
+                                [ring/ring-devel "1.5.0"]
+                                [prone "1.1.1"]
+                                [luminus-jetty "0.1.4"]
+                                [directory-naming/naming-java "0.8"]]
+                 :repl-options {:init-ns user}
+                 :source-paths ["env/dev/clj"]
+                 :resource-paths ["env/dev/resources"]}
+   :profiles/dev {}})
