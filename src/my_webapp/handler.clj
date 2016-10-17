f3818dc0af7a8ec87d7082a08f4f030ba967ef1d
Alexander Kozlov <asmailiks@gmail.com>
2016-10-17 11:31:21 +0300

    Add staff for container initialization and logging

--- src/my_webapp/handler.clj
+++ src/my_webapp/handler.clj
@@ -1,12 +1,40 @@
 (ns my-webapp.handler
   (:require [compojure.core :as cc]
+            [clojure.tools.logging :as log]
             #_[compojure.handler :as handler]
             [compojure.route :as route]
+            [luminus.logger :as logger]
+            [mount.core :as mount]
+            [my-webapp.env :refer [defaults]]
+            [my-webapp.config :refer [env]]
             [my-webapp.db :as db]
             [my-webapp.layout :as layout]
             [my-webapp.views :as views]
             #_[ring.adapter.jetty :as jetty])
   (:gen-class))
+
+(mount/defstate init-app
+  :start ((or (:init defaults) identity))
+  :stop ((or (:stop defaults) identity)))
+
+(mount/defstate log
+  :start (logger/init (:log-config env)))
+
+(defn init
+  "init will be called once when app is deployed as a servlet on an
+  app server such as Tomcat put any initialization code here"
+  []
+  (doseq [component (:started (mount/start))]
+    (log/info component "started")))
+
+(defn destroy
+  "destroy will be called when your application shuts down, put any
+  clean up code here"
+  []
+  (doseq [component (:stopped (mount/stop))]
+    (log/info component "stopped"))
+  (shutdown-agents)
+  (log/info "mpm-web has shut down!"))
 
 (cc/defroutes app-routes
   (cc/GET "/"
