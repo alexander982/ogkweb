4c4a2caaec8a67e2f20f93496c34e4d581c888a8
Alexander Kozlov <asmailiks@gmail.com>
2017-01-18 11:50:53 +0300

    Fix namespaces
    Damned copy&paste :)

--- src/my_webapp/db/core.clj
+++ src/my_webapp/db/core.clj
@@ -1,16 +1,16 @@
-(ns mpm-web.db.core
+(ns my-webapp.db.core
   (:require
     [conman.core :as conman]
     [mount.core :refer [defstate]]
-    [mpm-web.config :refer [env]]))
+    [my-webapp.config :refer [env]]))
 
 (defstate ^:dynamic *db*
-          :start (conman/connect!
-                   {:datasource
-                    (doto (org.h2.jdbcx.JdbcDataSource.)
-                          (.setURL (env :database-url))
-                          (.setUser "sa")
-                          (.setPassword ""))})
+  :start (conman/connect!
+          {:datasource
+           (doto (org.h2.jdbcx.JdbcDataSource.)
+             (.setURL (env :database-url))
+             (.setUser "sa")
+             (.setPassword ""))})
   :stop (conman/disconnect! *db*))
 
 (conman/bind-connection *db*
