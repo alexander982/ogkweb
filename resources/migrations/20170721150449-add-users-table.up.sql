1026da4fec6c4a8e4dd213c1d61cfe8219f9666c
Alexander Kozlov <asmailiks@gmail.com>
2017-07-24 15:56:18 +0300

    Fix users table DDL schema

--- resources/migrations/20170721150449-add-users-table.up.sql
+++ resources/migrations/20170721150449-add-users-table.up.sql
@@ -1,11 +1,12 @@
 create table users
-(id long primary key,
+(id long identity primary key,
+ login varchar(30) not null unique,
  first_name varchar(30),
  last_name varchar(30),
  email varchar(30),
- admin boolean,
+ admin boolean default false,
  last_login time,
  last_ip varchar(15),
- is_active boolean,
+ is_active boolean default true,
  pass varchar(300),
  remember_token varchar(60));
