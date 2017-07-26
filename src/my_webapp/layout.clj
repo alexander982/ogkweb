696b670c6bb6d8d2bc5b4de71027f68a032afb21
Alexander Kozlov <asmailiks@gmail.com>
2017-07-26 11:52:20 +0300

    Add csrf tag to renderer

--- src/my_webapp/layout.clj
+++ src/my_webapp/layout.clj
@@ -1,11 +1,14 @@
 (ns my-webapp.layout
   (:require [selmer.parser :as parser]
             [selmer.filters :as filters]
-            [ring.util.http-response :refer [content-type ok]]))
+            [ring.util.http-response :refer [content-type ok]]
+            [ring.util.anti-forgery :refer [anti-forgery-field]]
+            [ring.middleware.anti-forgery :refer [*anti-forgery-token*]]))
 
 (declare ^:dynamic *identity*)
 (declare ^:dynamic *app-context*)
 (parser/set-resource-path! (clojure.java.io/resource "templates"))
+(parser/add-tag! :csrf-field (fn [_ _] (anti-forgery-field)))
 
 (defn render
   [template & [params]]
@@ -16,6 +19,7 @@
      (assoc params
             :page template
             :identity *identity*
+            :csrf-token *anti-forgery-token*
             :servlet-context *app-context*)))
    "text/html; charset=utf-8"))
 
