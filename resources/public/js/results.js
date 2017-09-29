9298e393cffb47f3874bdf363274afb22bdbc347
Alexander Kozlov <asmailiks@gmail.com>
2017-09-29 14:12:27 +0300

    Add function to show popup menu on table row mouse enter

--- resources/public/js/results.js
+++ resources/public/js/results.js
@@ -11,3 +11,27 @@ function selectOneRow (e){
                                    });
     $('#popup').dialog('open');
 }
+
+function enterRow(e) {
+    $('tr.selection').toggleClass('selection');
+    var popup = $('#popup');
+    var offset = $(this).offset();
+    var ttop = 0;
+    if ((popup.height() + offset.top + 5) > $(document).height()) {
+        ttop = $(document).height() - popup.height() - 15;
+    } else {
+        ttop = offset.top;
+    }
+    var id = $(this).data('id');
+    $('#popup a').each(function () {
+        var href = $(this).attr('href');
+        $(this).attr('href', href.slice(0, href.indexOf('=') +1) + id);
+    });
+    popup.css({
+        position: 'absolute',
+        left: offset.left + $(this).width() + 8 + 'px',
+        top: ttop + 'px'
+    });
+    popup.show();
+    $(this).addClass('selection');
+}
