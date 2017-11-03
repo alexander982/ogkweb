5a89c7158b8465dd826798ee1ce7a292b64f5af4
Alexander Kozlov <asmailiks@gmail.com>
2017-11-03 13:48:35 +0300

    Remove unused function

--- resources/public/js/results.js
+++ resources/public/js/results.js
@@ -1,17 +1,3 @@
-function selectOneRow (e){
-    $('tr.selection').toggleClass('selection');
-    $(this).toggleClass('selection');
-    var id = $(this).data('id');
-    $('#popup a').each(function () {
-        var href = $(this).attr('href');
-        $(this).attr('href', href.slice(0, href.indexOf('=') +1) + id);
-    });
-    $('#popup').dialog('option', { position: {my: 'left top+12',
-                                              of: e}
-                                   });
-    $('#popup').dialog('open');
-}
-
 function enterRow(e) {
     $('tr.selection').toggleClass('selection');
     var bottomOffset = 15;
