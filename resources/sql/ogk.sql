e006aed4149f123305518b296073302b702d6a77
Alexander Kozlov <asmailiks@gmail.com>
2018-10-10 09:51:12 +0300

    Fix get metals request
    A problem with empty return values was not because of conman library.
    The poin was in H2 recursive query limitations and SQL parameters
    binding by HugSQL. The problem is solved by using user defined vars in
    H2 database. This fix add update-id-param query, update get-metals-by-id
    query and remove unused get-metals.

--- resources/sql/ogk.sql
+++ resources/sql/ogk.sql
@@ -32,31 +32,16 @@ select u2.id, u2.prefix, u2.num, u2.name
 from (unit u inner join contain c on u.id = c.unit_id) inner join unit u2 on c.cont_id = u2.id 
 where u.id = :id;
 
+--:name update-id-param :!
+SET @id_=:id;
+
 --:name get-metals-by-id :? :1
 with recursive sostav (pref, num, name, qnt, gold, silver, pl, pal, id) as (
     SELECT u2.prefix, u2.num, u2.name, c.qnt,
         u2.gold, u2.silver, u2.pl, u2.pal, u2.id
     FROM (UNIT u inner join contain c on u.id = c.cont_id)
     inner join unit u2 on u2.id  = c.unit_id
-    where u.id = :id
-    union all
-    select u.prefix, u.num, u.name, c.qnt, u.gold, u.silver, u.pl, u.pal, u.id
-    from ( sostav s inner join contain c on s.id = c.cont_id)
-    inner join unit u on c.unit_id = u.id)
-select sum(cast(gold as double)*cast(qnt as int)) as gold,
-       sum(cast(silver as double)*cast(qnt as int)) as silver,
-       sum(cast(pl as double)*cast(qnt as int)) as pl,
-       sum(cast(pal as double)*cast(qnt as int)) as pal
-from sostav;
-
---:name get-metals-by-pref-num :? :1
-with recursive sostav (pref, num, name, qnt, gold, silver, pl, pal, id) as (
-    SELECT u2.prefix, u2.num, u2.name, c.qnt,
-        u2.gold, u2.silver, u2.pl, u2.pal, u2.id
-    FROM (UNIT u inner join contain c on u.id = c.cont_id )
-    inner join unit u2 on u2.id  = c.unit_id
-    where u.id  =  (select id from unit
-        where prefix like :pref and num like :num)
+    where u.id = @id_
     union all
     select u.prefix, u.num, u.name, c.qnt, u.gold, u.silver, u.pl, u.pal, u.id
     from ( sostav s inner join contain c on s.id = c.cont_id)
