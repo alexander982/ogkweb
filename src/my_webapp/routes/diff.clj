0422284f468ddc0a6c77eee59486171a3d11ef10
Alexander Kozlov <asmailiks@gmail.com>
2018-10-10 11:16:41 +0300

    Update routes.diff to use db.core

--- src/my_webapp/routes/diff.clj
+++ src/my_webapp/routes/diff.clj
@@ -1,19 +1,20 @@
 (ns my-webapp.routes.diff
   (:require [clojure.tools.logging :as log]
             [compojure.core :refer [defroutes GET]]
-            [my-webapp.db :as db]
+            [my-webapp.db.core :as db]
             [my-webapp.layout :as layout]
             [ring.util.http-response :refer [found]]))
 
 (defn diff-page [id1 id2]
-  (let [u1 (db/get-unit-by-id id1)
-        u2 (db/get-unit-by-id id2)]
-    (layout/render "diff/results.html" {:results (db/get-diff-by-ids
-                                                  id1 id2)
-                                        :unit1 u1
-                                        :unit2 u2
-                                        :db-update-date
-                                        (:v_date (first (db/get-version-date)))})))
+  (let [u1 (db/get-unit-by-id {:id id1})
+        u2 (db/get-unit-by-id {:id id2})]
+    (layout/render "diff/results.html"
+                   {:results (db/get-diff-by-ids
+                              {:id1 id1 :id2 id2})
+                    :unit1 u1
+                    :unit2 u2
+                    :db-update-date
+                    (:v_date (db/get-last-db-update))})))
 
 (defroutes diff-routes
   (GET "/diff/results" [id1 id2] (diff-page id1 id2)))
