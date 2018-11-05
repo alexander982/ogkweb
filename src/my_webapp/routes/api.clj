d7c825533c7410b84bd25d440b714997edca2a75
Alexander Kozlov <asmailiks@gmail.com>
2018-11-05 12:11:02 +0300

    Update my-webapp.routes.api to use my-webapp.db.core

--- src/my_webapp/routes/api.clj
+++ src/my_webapp/routes/api.clj
@@ -1,5 +1,5 @@
 (ns my-webapp.routes.api
-  (:require [my-webapp.db :as db]
+  (:require [my-webapp.db.core :as db]
             [compojure.core :refer [defroutes GET]]
             [ring.util.http-response :refer [content-type ok]]))
 
@@ -8,7 +8,7 @@
 (defn get-docs
   [fname]
   {:archive archive-path
-   :docs (db/get-docs-by-fname fname)})
+   :docs (db/get-docs-by-fname {:fname (str "%" fname "%")})})
 
 (defroutes api-routes
   (GET "/api/docs" [fname]
