4b008e6b9c80bb8ea43f28153631573a6b447ec0
Alexander Kozlov <asmailiks@gmail.com>
2018-12-06 11:13:42 +0300

    Add main admin and updates page routes

--- src/my_webapp/routes/admin.clj
+++ src/my_webapp/routes/admin.clj
@@ -4,11 +4,20 @@
             [clojure.tools.logging :as log]
             [compojure.core :refer [defroutes GET POST]]))
 
+(defn admin-page
+  []
+  (layout/render "admin/admin.html"))
+
 (defn users-page
   []
   (layout/render "admin/users.html" {:users (db/get-users {:limit 10
                                                            :offset 0})}))
 
+(defn updates-page
+  []
+  (layout/render "admin/updates.html" {:updates (db/get-updates)}))
+
 (defroutes admin-routes
-  (GET "/admin/users" []
-       (users-page)))
+  (GET "/admin" [] (admin-page))
+  (GET "/admin/users" [] (users-page))
+  (GET "/admin/updates" [] (updates-page)))
