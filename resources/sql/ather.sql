bbaa5e798302ac897d46f2a6ae83e145caf46fed
Alexander Kozlov <asmailiks@gmail.com>
2018-12-06 11:21:43 +0300

    Add updates query

--- resources/sql/ather.sql
+++ resources/sql/ather.sql
@@ -1,3 +1,8 @@
 --:name get-last-db-update :? :1
 SELECT v_date FROM versions
-WHERE v_id = (SELECT cv_id from cversion)
+WHERE v_id = (SELECT cv_id from cversion);
+
+--:name get-updates :? :*
+select * from versions
+order by v_date desc
+limit 10;
