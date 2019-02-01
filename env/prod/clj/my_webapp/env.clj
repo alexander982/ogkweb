92acfe740f42d7aeec1dfe647ba8bc267aaeb63c
Alexander Kozlov <asmailiks@gmail.com>
2019-02-01 08:26:00 +0300

    Add database migration at system startup

--- env/prod/clj/my_webapp/env.clj
+++ env/prod/clj/my_webapp/env.clj
@@ -1,9 +1,16 @@
 (ns my-webapp.env
-  (:require [clojure.tools.logging :as log]))
+  (:require [clojure.tools.logging :as log]
+            [my-webapp.config :refer [env]]
+            [luminus-migrations.core :as migrations]))
 
 (def defaults
   {:init
    (fn []
+     (let [db-url (select-keys env [:database-url])]
+       (when (> (count (migrations/migrate ["pending"]
+                                         db-url))
+              0)
+         (migrations/migrate ["migrate"] db-url)))
      (log/info "\n-=[ogk-web started successfully]=-"))
    :stop
    (fn []
