1e81e2c8048aab16aece287127fdb72f71938bf9
Alexander Kozlov <asmailiks@gmail.com>
2019-04-02 08:50:42 +0300

    Add unit paremeters values queries

--- resources/sql/params.sql
+++ resources/sql/params.sql
@@ -15,3 +15,19 @@ where id = :id;
 
 --:name delete-param! :! :n
 delete from params where id = :id;
+
+--:name get-unit-params :? :*
+select * from unit_param inner join params on unit_param.param_id = params.id
+where unit_param.unit_id = :id;
+
+--:name add-unit-params! :! :n
+insert into unit_param
+(unit_id, param_id, value, updated_by)
+values (:unit-id, :param-id, :value, :updated-by);
+
+--:name update-unit-param! :! :n
+update unit_param set value = :value, updated_by = :updated-by
+where unit_id = :unit-id and param_id = :param-id;
+
+--:name delete-unit-param! :! :n
+delete from unit_param where unit_id = :unit-id and param_id = :param-id;
