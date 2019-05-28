2e5b8fe8f7be32bea2b35bb4539616ca43ae0e61
Alexander Kozlov <asmailiks@gmail.com>
2019-05-28 10:40:10 +0300

    Add materials query

--- resources/sql/ather.sql
+++ resources/sql/ather.sql
@@ -6,3 +6,9 @@ WHERE v_id = (SELECT cv_id from cversion);
 select * from versions
 order by v_date desc
 limit 10;
+
+--:name get-materials :? :*
+select am.class_, am.name, am.article, am.flow, mu.short_name from
+meas_units mu inner join aux_materials am on mu.id = am.meas_unit_id
+where unit_id = :id
+order by class_;
