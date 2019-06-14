667abc49042ed7ae1b9d29f6990a8fbe6cd52459
Alexander Kozlov <asmailiks@gmail.com>
2019-06-14 09:38:46 +0300

    Add params queries for admin reprts

--- resources/sql/admin.sql
+++ resources/sql/admin.sql
@@ -1,3 +1,14 @@
 --:name backup-database :? :*
 script nosettings drop
 to 'backup.sql' table users, cversion, versions, params, unit_param;
+
+--:name get-all-params :? :*
+select * from params;
+
+--:name get-values :? :*
+select p.name as param, u.prefix, u.num, u.name, v.value, v.updated, us.login
+from ((users us inner join unit_param v on v.updated_by = us.id)
+ inner join params p on p.id = v.param_id)
+ inner join unit u on u.id = v.unit_id
+order by v.updated
+limit 50;
