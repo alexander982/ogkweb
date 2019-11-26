2cc0b1181f29d2c153206a295b2dafc2c33d8f60
Alexander Kozlov <asmailiks@gmail.com>
2019-11-26 16:04:20 +0300

    Change value sorting

--- resources/sql/admin.sql
+++ resources/sql/admin.sql
@@ -10,5 +10,5 @@ select p.name as param, u.prefix, u.num, u.name, v.value, v.updated, us.login
 from ((users us inner join unit_param v on v.updated_by = us.id)
  inner join params p on p.id = v.param_id)
  inner join unit u on u.id = v.unit_id
-order by v.updated
+order by v.updated desc
 limit 50;
